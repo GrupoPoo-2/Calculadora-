5311e09eb40c61c5493b0d125fad6d8de0ba3434
GrupoPoo-2 <167116265+GrupoPoo-2@users.noreply.github.com>
2024-04-21 00:14:04 -0500

    Add files via upload

---
+++ src/sesion/pkg4/Sesion4.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sesion.pkg4;
+
+import Pantalla.PantallaCalculadora;
+
+/**
+ *
+ * @author migueldedios
+ */
+public class Sesion4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        PantallaCalculadora pantalla = new PantallaCalculadora();
+        pantalla.setVisible(true);
+        
+    }
+    
+}
